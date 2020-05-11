7efe55a2840af2796494c4072be65a42b4646486
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-11 01:14:13 -0500

    added changes

---
+++ app.js
@@ -0,0 +1,44 @@
+let name = 'Justin';
+// My name
+
+const states = 50;
+// Number of states in the U.S.
+
+let sum = 5+4;
+// The sum of 2 numbers
+
+function sayHello(){
+
+}
+
+sayHello('Hello World!');
+
+let vegetables = ['Broccoli', 'Carrot', 'Avocado', 'Celery', 'Collards'];
+
+let pet = {
+    name: 'Brock',
+    breed: 'Bulldog'
+};
+
+let people = [
+    {
+        name: 'Dawn',
+        age: 29
+    },
+    {
+        name: 'Jason',
+        age: 19
+    },
+    {
+        name: 'Chris',
+        age: 50
+    },
+    {
+        name: 'Will',
+        age: 13
+    },
+    {
+        name: 'Zach',
+        age: 24
+    }
+];
