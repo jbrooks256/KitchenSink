13bb3e81856632f52f9472aadedd80b10bf83d40
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-11 14:56:39 -0500

    made more changes

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-let name = 'Justin';
+let myName = 'Justin';
 // My name
 
 const states = 50;
 // Number of states in the U.S.
 
-let sum = 5+4;
+let sum = 5 + 4;
 // The sum of 2 numbers
 
-function sayHello(){
-
+function sayHello() {
+    alert('Hello World!');
 }
 
-sayHello('Hello World!');
+sayHello();
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
+    }
+};
+
+checkAge("Chuck", 34);
+checkAge("Lance", 15);
+checkAge("Matt", 20);
+checkAge("Brad", 23);
 
 let vegetables = ['Broccoli', 'Carrot', 'Avocado', 'Celery', 'Collards'];
+
+for (let i = 0; i < vegetables.length; i++) {
+    console.log(vegetables[i]);
+
+}
 
 let pet = {
     name: 'Brock',
@@ -42,3 +58,7 @@ let people = [
         age: 24
     }
 ];
+
+function getLength(word) {
+    return word.length;
+}
